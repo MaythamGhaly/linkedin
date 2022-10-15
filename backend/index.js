c67d1f7eb2a2d8adab2147340a1ced8501bbac18
Maytham Ghaly <maythamghaly@gmail.com>
2022-10-15 21:03:25 +0300

    backend: add connection to db

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('dotenv').config();
+require('./config/db.config')
+const app = express();
+app.use(express.json());
+
+// const authRoutes = require('./routes/auth.routes');
+// app.use('/auth', authRoutes)
+
+// const usersRoutes = require('./routes/users.routes');
+// app.use('/users', usersRoutes)
+
+app.listen(process.env.PORT, (err)=>{
+    if(err) throw err;
+    console.log(`server running on port ${process.env.PORT}`);
+})
