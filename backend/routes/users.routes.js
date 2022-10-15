5b1e95055942a3f4f767399161b07d7b4946ec6b
Maytham Ghaly <maythamghaly@gmail.com>
2022-10-16 01:36:33 +0300

    backend: add authmiddleware

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,10 +1,11 @@
 const {Router} = require('express');
 const { getUserInformation } = require('../controllers/user.controller')
 const {create} = require('../controllers/information.controller')
+const authMiddleware = require('../middlewares/auth.middleware')
 const router = Router();
 
-router.post('/get-user-information/:id', getUserInformation);
-router.post('/add-info/:id', create);
+router.post('/get-user-information/:id',authMiddleware, getUserInformation);
+router.post('/add-info/:id',authMiddleware, create);
 
 
 module.exports = router;
