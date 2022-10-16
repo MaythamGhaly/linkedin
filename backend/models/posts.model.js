7a00cf45cb37c9fb0c84d392ecfd2a5d7c5b9dbe
Maytham Ghaly <maythamghaly@gmail.com>
2022-10-16 14:53:29 +0300

    backend: add posts model

---
+++ backend/models/posts.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const postsSchema = new mongoose.Schema({
+    job_title: {
+        type: String,
+        required: 'job_title is required'
+    },
+    job_descreption: {
+        type: String,
+        required: 'job_descreption is required'
+    },
+    specifics_of_the_job_role: {
+        type: String,
+        required: 'specifics_of_the_job_role is required'
+    },
+    requirements:{
+        type: String,
+        required: 'requirements is required'
+    },
+    user :{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    }
+})
+
+module.exports = mongoose.model('Post',postsSchema);
